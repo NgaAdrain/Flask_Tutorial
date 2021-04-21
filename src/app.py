9a043f648b899d1113e5e7db53a57306415320ab
Adrain <adrain@nga.com>
2021-04-21 16:41:35 +0900

    login까지 끝

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from flask import request
 from flask import redirect
 #from data import Articles
 import pymysql
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 app.debug = True #개발 때 만
@@ -109,6 +110,54 @@ def article(id):
     #    if article['id'] == int(id):
     #        return render_template('article.html',article = article)
     return render_template('article.html',article = topic)
+
+@app.route('/register', methods=["GET","POST"])
+def register(): 
+    if request.method == "POST": #form으로 부터 메소드 전달.
+        cursor = db.cursor()
+        u_name = request.form['U_Name']
+        sql = "SELECT id FROM users WHERE username = %s;"
+        cursor.execute(sql,u_name)
+        profile = cursor.fetchall()
+        if len(profile) != 0:
+            #중복된 아이디 입니다를 출력
+            return redirect("/register")
+        pswd = sha256_crypt.hash(request.form['PSWD'])
+        if sha256_crypt.verify(request.form['PSWD_chk'],pswd) != True:
+            return redirect("/register")
+        name = request.form['Name'] #원하는거
+        email = request.form['Email']
+        sql = "INSERT INTO users (name,email,username,password) VALUES (%s,%s,%s,%s);" #삽입 쿼리
+        input_data = [name,email,u_name,pswd]
+        #print(request.form['desc'])
+        cursor.execute(sql,input_data)
+        db.commit()
+        return redirect("/data/test") #추가 완료
+    else:
+        return render_template("register.html") #form으로 부터 메소드 전달.
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+    if request.method == "POST": #form으로 부터 메소드 전달.
+        cursor = db.cursor()
+        name = request.form['U_Name'] #원하는거
+        sql = "SELECT id, password FROM users WHERE username = %s;" #삽입 쿼리
+        cursor.execute(sql,name)
+        profile = cursor.fetchall()
+        pswd = request.form['PSWD']
+        
+        if len(profile) != 0:
+            if sha256_crypt.verify(pswd,profile[0][1]):
+                #로그인 성공 출력
+                return redirect("/data/test")
+            else:
+                #비번 틀림
+                return redirect("/login")
+        else :
+            #아이디 틀림
+            return redirect("/login")
+    else:
+        return render_template("login.html")
 
 if __name__ == '__main__':
     app.run()
